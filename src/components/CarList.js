fa6e6e52a9fa91e540529cd2f4c627201c991912
Parv-gugnani <9765.parv@gmail.com>
2023-09-12 17:51:43 +0530

    handledeltecar

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const CarList = () => {
-  return <div>CarList</div>;
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
+
+  const handleCarDelete = (car) => {
+    //
+  };
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 };
 
 export default CarList;
