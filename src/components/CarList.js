07e6a587d657c582b22bb2bf628550a879c1770c
Parv-gugnani <9765.parv@gmail.com>
2023-09-12 17:56:29 +0530

    bulma

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
 
 const CarList = () => {
+  const dispatch = useDispatch();
   const cars = useSelector((state) => {
     return state.cars.data;
   });
 
   const handleCarDelete = (car) => {
-    //
+    dispatch(removeCar(car.id));
   };
 
   const renderedCars = cars.map((car) => {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "bulma/css/bulma.css";
+import "./styles.css";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
