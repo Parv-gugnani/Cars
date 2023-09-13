55be80d1fe87df4c6e7df5ea5899caf610729aa0
Parv-gugnani <9765.parv@gmail.com>
2023-09-13 09:20:44 +0530

    car list

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -4,8 +4,10 @@ import { removeCar } from "../store";
 
 const CarList = () => {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => {
-    return state.cars.data;
+  const cars = useSelector(({ cars: { data, searchTerm } }) => {
+    return data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   });
 
   const handleCarDelete = (car) => {
--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,7 +1,30 @@
 import React from "react";
+import useDispatch from "react-redux";
+import { changeSearchTerm, searchTermChange } from "../store";
+import useSelector from "react-redux";
 
 const CarSearch = () => {
-  return <div>CarSearch</div>;
+  const dispatch = useDispatch();
+  const searchTerm = useSelector((state) => {
+    return state.cars.searchTerm;
+  });
+
+  const handleSearchTermChange = (event) => {
+    dispatch(changeSearchTerm(event.target.value));
+  };
+  return (
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field is-horizontal">
+        <label className="label">Search</label>
+        <input
+          className="input"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        ></input>
+      </div>
+    </div>
+  );
 };
 
 export default CarSearch;
